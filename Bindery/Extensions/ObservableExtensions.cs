4f14e3a1f482ee9a2b07f883e06e7a404574aeb2
Dave Perry <dave@slugburn.com>
2020-05-02 16:31:39 -0700

    Removed Send, Post methods from ObservableExtensions

--- Bindery/Extensions/ObservableExtensions.cs
+++ Bindery/Extensions/ObservableExtensions.cs
@@ -3,61 +3,12 @@ using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bindery.Extensions
 {
     public static class ObservableExtensions
     {
-        /// <summary>
-        /// Send a value to an IObservable that also implements IObserver, such as Subject
-        /// </summary>
-        /// <typeparam name="T">The value type</typeparam>
-        /// <param name="observable">The observable</param>
-        /// <param name="value">The value</param>
-        /// <exception cref="ArgumentException">The observable does not implement IObserver</exception>
-        public static void Send<T>(this IObservable<T> observable, T value)
-        {
-            if (!(observable is IObserver<T> observer))
-                throw new ArgumentException("The observable must also implement IObserver", nameof(observable));
-            observer.OnNext(value);
-        }
-
-        /// <summary>
-        /// Synchronously send a value to an IObservable that also implements IObserver, such as Subject
-        /// </summary>
-        /// <typeparam name="T">The value type</typeparam>
-        /// <param name="observable">The observable</param>
-        /// <param name="value">The value</param>
-        /// <param name="synchronizationContext">A synchronization context</param>
-        /// <exception cref="ArgumentException">The observable does not implement IObserver</exception>
-        public static void Send<T>(this IObservable<T> observable, T value, SynchronizationContext synchronizationContext)
-        {
-            if (!(observable is IObserver<T> observer))
-                throw new ArgumentException("The observable must also implement IObserver", nameof(observable));
-            if (synchronizationContext == null)
-                throw new ArgumentNullException(nameof(synchronizationContext));
-            synchronizationContext.Send(_ => observer.OnNext(value), null);
-        }
-
-        /// <summary>
-        /// Asynchronously post a value to an IObservable that also implements IObserver, such as Subject
-        /// </summary>
-        /// <typeparam name="T">The value type</typeparam>
-        /// <param name="observable">The observable</param>
-        /// <param name="value">The value</param>
-        /// <param name="synchronizationContext">A synchronization context</param>
-        /// <exception cref="ArgumentException">The observable does not implement IObserver</exception>
-        public static void Post<T>(this IObservable<T> observable, T value, SynchronizationContext synchronizationContext)
-        {
-            if (!(observable is IObserver<T> observer))
-                throw new ArgumentException("The observable must also implement IObserver", nameof(observable));
-            if (synchronizationContext == null)
-                throw new ArgumentNullException(nameof(synchronizationContext));
-            synchronizationContext.Post(_ => observer.OnNext(value), null);
-        }
-
         public static IObservable<Unit> CreateObservableForAsyncAction<TArg>(this IObservable<TArg> observable, Func<TArg, Task> asyncAction, IScheduler scheduler)
         {
             Func<TArg, IObservable<Unit>> observableFactory;
